c72a53e06e2644a8a18788d4f2e723aa1d3e8b4f
Nadezhda Galchuk <n.galchuk@vkteam.ru>
2025-02-10 13:05:34 +0300

    build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,8 +22,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Nvgalch-test', // Usually your GitHub org/user name.
+  projectName: 'nvgalch-docusaurus', // Usually your repo name.
+  deploymentBranch: 'deployment',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
