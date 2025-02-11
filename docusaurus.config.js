9ca0e84485494784fcecaba96d741a425673ac30
Nadezhda Galchuk <n.galchuk@vkteam.ru>
2025-02-11 11:21:54 +0300

    create pages and style

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,9 +10,9 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'Демка',
+  tagline: 'Документация для разработчиков мини-приложений и игр',
+  favicon: 'img/vk-short.svg',
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
@@ -32,10 +32,26 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
+    defaultLocale: 'ru',
     locales: ['en', 'ru'],
+    path: 'i18n',
+    localeConfigs: {
+      en: {
+        label: 'English',
+        direction: 'ltr',
+        htmlLang: 'en-US',
+        calendar: 'gregory',
+        path: 'en',
+      },
+      ru: {
+        label: 'Русский',
+        direction: 'ltr',
+        htmlLang: 'ru-RU',
+        calendar: 'gregory',
+        path: 'ru',
+      },
+    },
   },
-
   presets: [
     [
       'classic',
@@ -45,8 +61,8 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
@@ -56,8 +72,8 @@ const config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -76,22 +92,26 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: '',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/vk.svg',
         },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Документация',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          //{to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
+            href: 'https://dev.vk.com/ru/guide',
+            label: 'На портал',
+            position: 'right',
+          },
+          {
+            type: 'localeDropdown',
             position: 'right',
           },
         ],
@@ -103,20 +123,20 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Документация',
                 to: '/docs/intro',
               },
             ],
           },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-            ],
-          },
+//          {
+//            title: 'More',
+//            items: [
+//              {
+//                label: 'Blog',
+//                to: '/blog',
+//              },
+//            ],
+//          },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
---
+++ i18n/en/docusaurus-plugin-content-docs/current/apps/games.md
@@ -0,0 +1,42 @@
+---
+sidebar_position: 2
+---
+
+# VK Games
+
+**VK Games** is a gaming platform for VKontakte users. Players can easily launch games from the interface, with no downloads, installations or registrations required.
+
+Create and publish a game on VKontakte and use free promotion methods to attract players.
+
+## Benefits for developers and publishers
+
+* 10+ million monthly active players.
+* 500+ VK API operations for integration.
+* Free promotion methods.
+* Recommendations and best practices from the platform.
+
+[Learn more](en/games/benefits)
+
+## First steps
+
+VK games are HTML5 applications for mobile devices and computers. Start by creating a simple game and adding it to the platform. [See our step-by-step guide](en/games/getting-started).
+
+## Promotion and monetization
+
+* [Publication in game catalog](en/games/catalog/moderation)
+* [Promotion](en/games/promotion/featured)
+* [Game purchases](en/games/monetization/digital-goods)
+* [Advertising in games](en/games/monetization/ad)
+
+## Support and help
+
+* [Frequently asked questions](en/games/faq)
+* [How to contact Support](en/games/support)
+* [Developer communities](en/games/support#Developer%20communities)
+
+## Useful links
+
+* [VK Bridge Library](en/games/bridge)
+* [VK API](https://dev.vk.com/reference)
+* [Best promotion practices](en/games/promotion/best-practices)
+* [Best monetization practices](en/games/monetization/best-practices)
